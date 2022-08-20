3a26d43dc1233433277ef26e0f9cc96a0228a39f
vishhy <sharma.vishwas7788@gmail.com>
2022-08-20 23:21:49 +0530

    This is my first code push

--- test1.py
+++ test1.py
@@ -13,11 +13,6 @@ d = {
     'mail': 'vishwas@mail.com',
     'surname': 'K. Sharma'
 }
-d = {
-    'name': 'vishwas',
-    'mail': 'vishwas@mail.com',
-    'surname': 'K. Sharma'
-}
 
 qury1 = connection['mongotest']
 coll1 = qury1['test']
